49b3215196f60c57ad192a4b8295bcbe774a931e
alessandrochillotti <alessandro.chillotti@outlook.it>
2021-03-27 16:29:34 +0100

    #1

---
+++ src/main/java/MavenTravis/MavenTravis/HelloWorld.java
@@ -0,0 +1,12 @@
+package MavenTravis.MavenTravis;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+	public static final Logger LOGGER = Logger.getLogger("Hello World!");
+	
+	public static void main(String[] args) {
+		LOGGER.info(LOGGER.getName());
+	}
+}
